45458e5875d1b8784362f4fe499e01f98c362f48
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-03-31 15:52:12 -0500

    better settings also go check out the Winter Wallpaper Pack on Century Packages

--- system/apps/settings.js
+++ system/apps/settings.js
@@ -1,9 +1,31 @@
+import { readFile } from "../important/fs";
 import { newFile } from "/system/important/fs.js";
 import { WindowCreator,renderWindow,betterPrompt } from "/system/ui/ui.js";
 const win = new WindowCreator
+function loadScript(code, callback) {
+    try {
+        const script = document.createElement('script');
+        script.textContent = code;
+        script.type = 'module'; // does this hurt no
+        script.onload = () => {
+            console.log(`Script loaded: ${code}`);
+            if (typeof callback === 'function') callback(code);
+        };
+        script.onerror = () => {
+            console.error(`Failed to load script: ${code}`);
+        };
+        document.head.appendChild(script);
+    } catch (err) {
+        console.error("Error loading script:", err);
+    }
+}
 win.newText("Settings")
 win.newSplit()
 win.newButton("Change main package repository",function(){
     betterPrompt("Repo?",function(val){newFile("/","repo",val)})
+})
+win.newSplit()
+win.newButton("Set wallpaper",function(){
+    loadScript(readFile("/apps/","Wallpaper Setter.js"))
 })
 renderWindow("Settings",win.output,400,300)
--- system/important/fs.js
+++ system/important/fs.js
@@ -464,13 +464,35 @@ win.newButton("Install!", function () {
 });
 
 renderWindow("Century Packages", win.output, 400, 300);
-`,"Settings.js":`import { newFile } from "/system/important/fs.js";
+`,"Settings.js":`import { readFile } from "../important/fs";
+import { newFile } from "/system/important/fs.js";
 import { WindowCreator,renderWindow,betterPrompt } from "/system/ui/ui.js";
 const win = new WindowCreator
+function loadScript(code, callback) {
+    try {
+        const script = document.createElement('script');
+        script.textContent = code;
+        script.type = 'module'; // does this hurt no
+        script.onload = () => {
+            console.log(\`Script loaded: \${code}\`);
+            if (typeof callback === 'function') callback(code);
+        };
+        script.onerror = () => {
+            console.error(\`Failed to load script: \${code}\`);
+        };
+        document.head.appendChild(script);
+    } catch (err) {
+        console.error("Error loading script:", err);
+    }
+}
 win.newText("Settings")
 win.newSplit()
 win.newButton("Change main package repository",function(){
     betterPrompt("Repo?",function(val){newFile("/","repo",val)})
+})
+win.newSplit()
+win.newButton("Set wallpaper",function(){
+    loadScript(readFile("/apps/","Wallpaper Setter.js"))
 })
 renderWindow("Settings",win.output,400,300)
 `}} };
