db741a2488cda1e9ec8be7bf4d7eaf4afd884d4f
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-03-31 14:19:23 -0500

    asdsasdasd

--- packager.py
+++ packager.py
@@ -1,6 +1,6 @@
 print("Packager for Century Packages")
-if True:
+with open(input("Filename? ")+".json","w") as f:
     installto = input("Install to? ")
     content = input("Content? (If the content is binary just base64 encode it) ")
     result = "{ 'installTo':'"+installto+"','contents':'"+content+"' }"
-    print(result)
+    f.write(result)
