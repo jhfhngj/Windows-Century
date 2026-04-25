6abcac2b4c8fdbbc2758ffb89d789178b2d40765
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-04-24 19:18:22 -0500

    alert but better

--- system/sounds/chord.js
+++ system/sounds/chord.js
@@ -1,4 +1,11 @@
+import { betterAlert } from "/system/ui/ui.js";
 window.chord = new Audio('/system/sounds/chord.mp3');
 window.addEventListener("error", () => {
     window.chord.play();
 });
+old = console.error
+console.error = function(data){
+    old(data)
+    window.chord.play()
+    betterAlert(`Error occurred: ${data}`)
+}
