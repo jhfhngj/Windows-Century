60a1670ae62f5e171af557d80a33c484300e2f1d
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-07-05 20:12:47 -0500

    awawawawa

--- system/apps/phonemode.js
+++ system/apps/phonemode.js
@@ -1,14 +1,14 @@
 import { newFile } from "/system/important/fs.js"
 import { WindowCreator, renderWindow } from "/system/ui/ui.js"
 const win = new WindowCreator
-win.newText("There is **no way** to switch back, except restarting Windows Century.\nPhone Mode is in **alpha** and may have too many bugs.\nAre you still sure?\n")
+win.newText("There is **no way** to switch back, except restarting Windows Century.\nPhone Mode is in **beta** and has many bugs.\nAre you still sure?\n")
 win.newButton("Y  e  s",function(){
     car.textContent += "Starting transformation..."
     car.textContent += "\nResize start menu"
     var menu = document.getElementById("0x001929FEFF")
     menu.style.width = window.innerWidth + "px"
-    menu.style.height = window.innerHeight + "px"
-    car.textContent += "Menu Yes."
+    menu.style.height = (window.innerHeight-20) + "px"
+    car.textContent += "\nMenu Yes."
     menu.style.opacity = "1"
     menu.style.pointerEvents = "all"
     car.textContent += "\nRemove window"
