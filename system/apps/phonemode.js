6ada67a7cae5fa7576f227cc8a1326da158d0dc7
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-07-05 20:05:57 -0500

    asd

--- system/apps/phonemode.js
+++ system/apps/phonemode.js
@@ -1,7 +1,7 @@
 import { newFile } from "/system/important/fs.js"
 import { WindowCreator, renderWindow } from "/system/ui/ui.js"
 const win = new WindowCreator
-win.newText("There is **no way** to switch back, except restarting Windows Century.\nPhone Mode is in **alpha** and may have bugs.\nAre you still sure?\n")
+win.newText("There is **no way** to switch back, except restarting Windows Century.\nPhone Mode is in **alpha** and may have too many bugs.\nAre you still sure?\n")
 win.newButton("Y  e  s",function(){
     car.textContent += "Starting transformation..."
     car.textContent += "\nResize start menu"
@@ -12,7 +12,7 @@ win.newButton("Y  e  s",function(){
     menu.style.opacity = "1"
     menu.style.pointerEvents = "all"
     car.textContent += "\nRemove window"
-    setInterval(win.remove(),2000)
+    setInterval(win.remove,2000)
 })
 const car = win.newText("debug log:\n")
 renderWindow("Are you sure?", win.output,100,100,"/system/cool photos/phonemode.png")
