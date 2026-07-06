8008de834334e670fbf07201829f70d6e30f41c2
Ezzeldin Ahmed <ezzeldinahmed1@outlook.com>
2026-07-05 20:03:06 -0500

    ccccccccccccccFIFA WORLD CUP =1

--- system/apps/phonemode.js
+++ system/apps/phonemode.js
@@ -1,5 +1,5 @@
-import { newFile } from "../important/fs"
-import { WindowCreator, renderWindow } from "../ui/ui"
+import { newFile } from "/system/important/fs.js"
+import { WindowCreator, renderWindow } from "/system/ui/ui.js"
 const win = new WindowCreator
 win.newText("There is **no way** to switch back, except restarting Windows Century.\nPhone Mode is in **alpha** and may have bugs.\nAre you still sure?\n")
 win.newButton("Y  e  s",function(){
@@ -8,6 +8,9 @@ win.newButton("Y  e  s",function(){
     var menu = document.getElementById("0x001929FEFF")
     menu.style.width = window.innerWidth + "px"
     menu.style.height = window.innerHeight + "px"
+    car.textContent += "Menu Yes."
+    menu.style.opacity = "1"
+    menu.style.pointerEvents = "all"
     car.textContent += "\nRemove window"
     setInterval(win.remove(),2000)
 })
